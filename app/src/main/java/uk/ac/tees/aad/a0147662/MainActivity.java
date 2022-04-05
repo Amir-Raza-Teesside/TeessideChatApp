d43ee058a58e22c24d2df469cc5ab94a8211e13e
Fattani Computers <amirraza2501@gmail.com>
2022-04-05 17:48:46 +0100

    Second

---
+++ app/src/main/java/uk/ac/tees/aad/a0147662/MainActivity.java
@@ -0,0 +1,16 @@
+package uk.ac.tees.aad.a0147662;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
