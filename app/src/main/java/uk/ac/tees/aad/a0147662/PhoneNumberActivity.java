9d0219ca5f7d897ad34c8feb23962ac7c60f20af
Fattani Computers <amirraza2501@gmail.com>
2022-04-06 03:15:18 +0100

    Created PhoneNumber activity with all designing

---
+++ app/src/main/java/uk/ac/tees/aad/a0147662/PhoneNumberActivity.java
@@ -0,0 +1,14 @@
+package uk.ac.tees.aad.a0147662;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhoneNumberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_number);
+    }
+}
