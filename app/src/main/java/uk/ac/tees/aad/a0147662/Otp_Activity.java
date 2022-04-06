74a0db865a0a40a32a65c032c3fb0d6b9e0a9e6f
Fattani Computers <amirraza2501@gmail.com>
2022-04-06 07:38:12 +0100

    Created OTP Activity Linked the project to Firebase Added the Google-services.json file

---
+++ app/src/main/java/uk/ac/tees/aad/a0147662/Otp_Activity.java
@@ -0,0 +1,24 @@
+package uk.ac.tees.aad.a0147662;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+
+
+
+public class Otp_Activity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp);
+
+
+
+
+    }
+}
