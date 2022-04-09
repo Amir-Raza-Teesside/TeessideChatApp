f54b31e01f3058d4625fb61c5c36891275e20b9e
Fattani Computers <amirraza2501@gmail.com>
2022-04-09 08:07:34 +0100

    Designed the Action Bar with custom List

--- app/src/main/java/uk/ac/tees/aad/a0147662/ContactsActivity.java
+++ app/src/main/java/uk/ac/tees/aad/a0147662/ContactsActivity.java
@@ -1,9 +1,15 @@
 package uk.ac.tees.aad.a0147662;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuBuilder;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class ContactsActivity extends AppCompatActivity {
 
@@ -17,9 +23,34 @@ public class ContactsActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+
+       switch (item.getItemId())
+       {
+
+           case R.id.tweet:
+               Toast.makeText(ContactsActivity.this,"JKHJKHKJK",Toast.LENGTH_LONG).show();
+               break;
+           case R.id.group:
+               Toast.makeText(ContactsActivity.this,"Create Group",Toast.LENGTH_LONG).show();
+               break;
+
+       }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressLint("RestrictedApi")
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
         getMenuInflater().inflate(R.menu.topmenu,menu);
-       return  true;
+        if(menu instanceof MenuBuilder){
+            MenuBuilder m = (MenuBuilder) menu;
+            m.setOptionalIconsVisible(true);
+        }
+
+        return true;
+
     }
 }
