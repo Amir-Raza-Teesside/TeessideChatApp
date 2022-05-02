c0dc07e5b979e8d45860781275ed67400f82ae88
Fattani Computers <amirraza2501@gmail.com>
2022-05-02 06:16:32 +0100

    Addes the Splash Screen

---
+++ app/src/main/java/uk/ac/tees/aad/a0147662/SplashActivity.java
@@ -0,0 +1,31 @@
+package uk.ac.tees.aad.a0147662;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private LottieAnimationView animationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+      //  animationView=findViewById(R.id.animation_view);
+      // animationView.playAnimation();
+
+        int SPLASH_TIME_OUT = 1500;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
